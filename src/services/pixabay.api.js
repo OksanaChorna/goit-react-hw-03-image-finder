ff835873f910b135e56eda47d0a8a609030764a6
OksanaChorna <ksuchorna@gmail.com>
2021-05-09 17:01:42 +0300

    Додано async

--- src/services/pixabay.api.js
+++ src/services/pixabay.api.js
@@ -11,8 +11,8 @@ axios.defaults.params = {
   per_page: 12,
 };
 
-const fetchPixabayImgs = ({ q, page }) => {
-  return axios.get('', { params: { q, page } });
+const fetchPixabayImgs = async ({ q, page }) => {
+  return await axios.get('', { params: { q, page } });
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
